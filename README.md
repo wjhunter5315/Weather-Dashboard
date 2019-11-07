08623232e49abae31d8ee5f63b399103c400e94b
wjhunter5315 <wjhunter5315@gmail.com>
2019-11-06 23:22:07 -0800

    cleaned up looks, works for basic weather

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Weather-Dashboard
 weather api homework
+
+Enter a City into the input field and click the button.
+See a Current Weather status as well as a 5 day forecast.
+
+I used some bootstrap for the bsic layout.
+Most of the elements are dynamically generated with JS.
+I understand how to use the API fairly well, im using 3 calls in one function.
+I currently do not have Moment.js on here, and I need some work with localStorage use.
+I also couldn't get the UV index color to change.
--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var APIKey = "039d220008220b797aca707e99972b77";
 $("#confirm").on("click", function() {
     var location = document.querySelector(".myLocation").value;
     localStorage.setItem('locationPicks', location);
-    var weatherURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + location + "&units=imperial&APPID=" + APIKey;
+    var weatherURL = "http://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=imperial&APPID=" + APIKey;
     $.ajax({
         url: weatherURL,
         method: "GET"
@@ -11,20 +11,19 @@ $("#confirm").on("click", function() {
     .then(function(response) {
             console.log(weatherURL);
             console.log(response);
-            var longNow = response.city.coord.lon;
+            var longNow = response.coord.lon;
             console.log(longNow);
-            var latNow = response.city.coord.lat;
+            var latNow = response.coord.lat;
             console.log(latNow);
-            var tempNow = response.list[0].main.temp;
+            var tempNow = response.main.temp;
             console.log(tempNow);
-            var humidNow = response.list[0].main.humidity;
+            var humidNow = response.main.humidity;
             console.log(humidNow);
-            var windNow = response.list[0].wind.speed;
+            var windNow = response.wind.speed;
             console.log(windNow);
-            var weatherIcon = response.list[0].weather[0].icon;
+            var weatherIcon = response.weather[0].icon;
             console.log(weatherIcon);
-            var forecastList = response.list;
-            console.log(forecastList);
+            
             var uvIndexURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latNow + "&lon=" + longNow + "&APPID=" + APIKey;
             $.ajax({
                 url: uvIndexURL,
@@ -34,30 +33,59 @@ $("#confirm").on("click", function() {
                 console.log(response);
                 var uvNow = response.value;
                 console.log(uvNow);
-                var uvBox = $("<div>");
+                var uvBox = $("<div>").attr("id", 'uvTarget');
+                // if (uvNow <= 2.9) {
+                //     document.text.getElementById("uvTarget").style.backgroundColor = '#00ff00';
+                // }
+                // else if(uvNow < 5.9 && uvNow > 3) {
+                //     document.getElementById("uvTarget").style.backgroundColor = '#ffff66';
+                // }
+                // else if(uvNow < 7.9 && uvNow > 6) {
+                //     document.getElementById("uvTarget").style.backgroundColor = '#ff9933';
+                // }
+                // else if(uvNow < 10.9 && uvNow > 8) {
+                //     document.getElementById("uvTarget").style.backgroundColor = '#ff0000';
+                // }
+                // else {
+                //     document.getElementById("uvTarget").style.backgroundColor = '#9900cc';
+                // }
                 uvBox.append("Temperature : " + tempNow + "F" + "<br>" + "Humidity : " + humidNow + "%" + "<br>" + "Wind Speed : " + windNow + "MPH" + "<br>" + "UV Index : " + uvNow);
                 $(".current-weather").append(uvBox);
+                var forecastURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + location + "&units=imperial&APPID=" + APIKey;
+                $.ajax({
+                    url: forecastURL,
+                    method: "GET"
+                })
+                .then(function(response) {
+                    console.log(response);
+                    var forecastList = response.list;
+                    console.log(forecastList);
+                    for (i=1; i<forecastList.length; i+=8) {
+                        var forecastDiv = $("<card>");
+                        var forecastDate = forecastList[i].dt_txt;
+                        console.log(forecastDate);
+                        var dateTrim = forecastDate.slice(0, 16);
+                        var forecastTemp = forecastList[i].main.temp;
+                        var forecastHumidity = forecastList[i].main.humidity;
+                        var forecastIcon = forecastList[i].weather[0].icon;
+                        var iconAt = "http://openweathermap.org/img/wn/" + forecastIcon + "@2x.png";
+                        var iconUsed = $("<img>").attr("src", iconAt);
+                        forecastDiv.append(dateTrim + "<br>" + "Temperature : " + forecastTemp + "F" + "<br>" + "Humidity : " + forecastHumidity + "%");
+                        $(".weather-forecast").append(forecastDiv);
+                        $(".weather-forecast").append(iconUsed);
+                    }
+
+                })
     
             });
 
-            for (i=7; i<forecastList.length; i+=8) {
-                var forecastDiv = $("<div>");
-                var forecastDate = forecastList[i].dt_txt;
-                console.log(forecastDate);
-                var forecastTemp = forecastList[i].main.temp;
-                var forecastHumidity = forecastList[i].main.humidity;
-                var forecastIcon = forecastList[i].weather[0].icon;
-                var iconAt = "http://openweathermap.org/img/wn/" + forecastIcon + "@2x.png";
-                var iconUsed = $("<img>").attr("src", iconAt);
-                forecastDiv.append(forecastDate + "<br>" + "Temperature : " + forecastTemp + "F" + "<br>" + "Humidity : " + forecastHumidity + "%");
-                $(".weather-forecast").append(forecastDiv);
-                $(".weather-forecast").append(iconUsed);
-            }
+            
+        console.log(location);
+        localStorage.getItem('locationPicks');
+        var prevSearch = $("<button>");
+        prevSearch.append(location);
+        $(".list-group").append(prevSearch);
+        localStorage.clear();
     });
 
 })
-// if (uvNow <= 2.9) {  green = 0 - 2.9
-// yellow = 3.0 - 5.9
-// orange = 6.0 - 7.9
-// red = 8.0 - 10.9
-// violet = 11.0+
